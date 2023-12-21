86a776ef3e32f92f5cfd78d800d46a9576a42233
Aril <146087408+ArilPakel@users.noreply.github.com>
2023-12-21 16:38:14 +0700

    Delete 231031081-aril-latihan 2.py

--- 231031081-aril-latihan 2.py
+++
@@ -1,6 +0,0 @@
-# Latihan 2
-inp = input('Masukkan suatu input: ')
-if len(inp) != 3:
- print('Panjang string ', len(inp),'tidak sama dengan 3')
-else:
- print('Panjang input sesuai yang diinginkan')
